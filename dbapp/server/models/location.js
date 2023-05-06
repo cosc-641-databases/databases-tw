4c1b3d742ade286d47e8b15413aa97d737f2ed1a
gregrholden <gregrholden@gmail.com>
2023-05-05 22:20:03 -0400

    Update comments in location.js to reflect correct model

--- dbapp/server/models/location.js
+++ dbapp/server/models/location.js
@@ -1,9 +1,8 @@
 const mongoose = require('mongoose');
 const uniqueValidator = require('mongoose-unique-validator');
 
-// Main schema for new User documents in MongoDB.
+// Main schema for new Locations documents in MongoDB.
 const locationSchema = new mongoose.Schema({
-  // Make usernames unique to create a MongoDB index on this field.
   name: { type: String, unique: false, required: true },
   uID: {type:String, unique: false, required: true },
   location: {
@@ -11,8 +10,5 @@ const locationSchema = new mongoose.Schema({
     lon: { type: Number, required: true }
   },
 });
-
-// Use 'mongoose-unique-validator' plugin to validate unique fields.
-locationSchema.plugin(uniqueValidator);
 
 module.exports = mongoose.model('Location', locationSchema);
